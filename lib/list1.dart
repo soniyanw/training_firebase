430fa304f2747d5f39cfed43784716e46879845b
soniyanw <soniya@nativewit.in>
2022-08-03 19:10:07 +0530

    Firebase added

--- lib/list1.dart
+++ lib/list1.dart
@@ -49,9 +49,3 @@ class _List1State extends State<List1> {
             }));
   }
 }
-/*git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/soniyanw/training_firebase.git
-git push -u origin main*/
